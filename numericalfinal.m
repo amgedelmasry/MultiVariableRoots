447a59fc226e9db918041d166ca42714297f15bf
amgedelmasry <85349889+amgedelmasry@users.noreply.github.com>
2023-01-02 05:09:10 +0200

    Gauss jordan done

--- numericalfinal.m
+++ numericalfinal.m
@@ -1,6 +1,6 @@
 f=uifigure('position',[20,30,800,800]);
 
-listbox=uilistbox(f,'Items',{'Gauss elimination','gauss seidle'},'position',[50 200 150 100]);
+listbox=uilistbox(f,'Items',{'Gauss elimination','Gauss seidle','Gauss jordan'},'position',[50 200 150 100]);
 gobutton=uibutton(f,'text','GO','ButtonPushedFcn',@(gobutton,event) getmethod(gobutton,listbox,f));
 global error_soln;
 error_soln=[];
@@ -10,69 +10,6 @@ global maxiterations;
 maxiterations=12;
 global counter;
 counter=0;
-%{
-n=3;
-stringat="";
-syms unknowns [n 1];
-equations=["12*a + 3*b -5*c - 1"];
-equations=[equations,"1*a + 5*b +3*c -28"];
-equations=[equations,"3*a + 7*b  + 13*c - 76"];
-solution=zeros(1,n);
-solution2=[1,0,1];
-app_error=0.008;
-error_soln=[];
-flag=1;
-maxiterations=12;
-counter=0;
-
-matrixOfCoeff=zeros(n,n+1);
-
-for j=1:n
-    str=strcat("@(",stringat,")",equations(j));
-    fc=str2func(str);
-    [b t]=coeffs(fc, unknowns);
-    for i=1:n
-      findIfZero=find(t==unknowns(i));
-      if(isempty(findIfZero))
-            matrixOfCoeff(j,i) = 0;
-      else
-        matrixOfCoeff(j,i) = b(findIfZero(1));
-      end
-    end
-    findIfZero=find(t==1);
-    if(isempty(findIfZero))
-            matrixOfCoeff(j,n+1) = 0;
-    else
-        matrixOfCoeff(j,n+1) = b(findIfZero(1));
-    end
-end
-
-
-while(flag==1 && counter<maxiterations)
-    for i=1:n
-        sum=0;
-        for j=1:n
-            if(j~=i)
-                sum=sum+matrixOfCoeff(i,j)*solution2(j);
-            end
-        end
-        sum=sum+matrixOfCoeff(i,n+1);
-        matrixOfCoeff(i,i);
-        sum=-sum/matrixOfCoeff(i,i);
-        err_soln(i)=(sum-solution2(i))/sum;
-        solution2(i)=sum
-    end
-    flag=0
-    for i=1:n
-        if(err_soln(i)>app_error)
-            flag=1;
-        end
-    end
-    counter=counter+1;
-end
-solution2
-%}
-f.Children
 function getmethod(gobutton,listbox,f)
 listbox.Value
     if strcmp(listbox.Value,'Gauss elimination')==1
@@ -81,13 +18,106 @@ listbox.Value
         label1=uilabel(f,'Text','Number of unknowns:','FontSize',12,'Position',[20 730 500 60]);
         textN=uieditfield(f,'FontSize',14,'Position',[150 750 50 15]);
         buttonN=uibutton(f,'FontSize',14,'text','ok','Position',[220 750 50 20],'ButtonPushedFcn',@(buttonN,event) getN(buttonN,textN,f));
-    elseif strcmp(listbox.Value,'gauss seidle')==1
+    elseif strcmp(listbox.Value,'Gauss seidle')==1
         delete(f.Children);
         label1=uilabel(f,'Text','Gauss seilde','FontSize',20,'Position',[175 760 300 50]);
         label1=uilabel(f,'Text','Number of unknowns:','FontSize',12,'Position',[20 730 500 60]);
         textN=uieditfield(f,'FontSize',14,'Position',[150 750 50 15]);
         buttonN=uibutton(f,'FontSize',14,'text','ok','Position',[220 750 50 20],'ButtonPushedFcn',@(buttonN,event) getNsiedle(buttonN,textN,f));
+    elseif strcmp(listbox.Value,'Gauss jordan')==1
+        delete(f.Children);
+        label1=uilabel(f,'Text','Gauss jordan','FontSize',20,'Position',[175 760 300 50]);
+        label1=uilabel(f,'Text','Number of unknowns:','FontSize',12,'Position',[20 730 500 60]);
+        textN=uieditfield(f,'FontSize',14,'Position',[150 750 50 15]);
+        buttonN=uibutton(f,'FontSize',14,'text','ok','Position',[220 750 50 20],'ButtonPushedFcn',@(buttonN,event) getNjordan(buttonN,textN,f));
     end
+end
+function getNjordan(buttonN,textN,f)
+    n=str2double(textN.Value);
+    syms unknowns [n 1];
+    stringat="";
+    for(i=1:n)
+        S=char(96+i);
+        s=S;
+        syms s;
+        unknowns(i)=S;
+        if(i~=n)
+            stringat=strcat(stringat,S,",");
+        else
+            stringat=strcat(stringat,S);
+        end
+    end
+    for i=1:n
+        label2=uilabel(f,'Text',strcat('eqn:',num2str(i)),'FontSize',12,'Position',[20 730-i*20 145 60]);
+        eqn(i)=uieditfield(f,'FontSize',14,'Position',[150 750-i*20 400 15])
+    end
+        calculate_btn=uibutton(f,'FontSize',14,'text','Calculate','Position',[220 750-(n+1)*30 100 20],'ButtonPushedFcn',@(calculate_btn,event) geteqnJordan(calculate_btn,f,n,eqn,stringat,unknowns));
+end
+function geteqnJordan(calculate_btn,f,n,eqn,stringat,unknowns)
+    for(i=1:n)
+        if i==1
+            equations=[string(eqn(i).Value)];
+        else
+            equations=[equations,string(eqn(i).Value)];
+        end
+    end
+    solution=[];
+    matrixOfCoeff=zeros(n,n+1);
+    for j=1:n
+        str=strcat("@(",stringat,")",equations(j));
+        fc=str2func(str);
+        [b t]=coeffs(fc, unknowns);
+        for i=1:n
+          findIfZero=find(t==unknowns(i));
+          if(isempty(findIfZero))
+                matrixOfCoeff(j,i) = 0;
+          else
+            matrixOfCoeff(j,i) = b(findIfZero(1));
+          end
+        end
+        findIfZero=find(t==1);
+        if(isempty(findIfZero))
+                matrixOfCoeff(j,n+1) = 0;
+        else
+            matrixOfCoeff(j,n+1) = b(findIfZero(1));
+        end
+    end
+    timestart=datevec(datestr(now,'dd-mm-yyyy HH:MM:SS.FFF'));
+    for i=1:n
+        if matrixOfCoeff(i,i)==0
+            fprintf("can't solve");
+            break;
+        else
+            for j=n:-1:i+1
+                if matrixOfCoeff(j,i)==0
+                    continue;
+                else
+                    ratio=matrixOfCoeff(j,i)/matrixOfCoeff(i,i);
+                    z=matrixOfCoeff(j,:)-ratio.*matrixOfCoeff(i,:);
+                    matrixOfCoeff(j,:)=z;
+                end
+            end
+            for v=i-1:-1:1
+                if matrixOfCoeff(v,i)==0
+                    continue;
+                else
+                    ratio=matrixOfCoeff(v,i)/matrixOfCoeff(i,i);
+                    z=matrixOfCoeff(v,:)-ratio.*matrixOfCoeff(i,:);
+                    matrixOfCoeff(v,:)=z;
+                end
+            end
+        end
+    end
+    for i=1:n
+       matrixOfCoeff(i,:)=matrixOfCoeff(i,:)./matrixOfCoeff(i,i);
+       solution(i)=matrixOfCoeff(i,n+1);
+    end
+    timeend=datevec(datestr(now,'dd-mm-yyyy HH:MM:SS.FFF'));
+    time=abs(etime(timestart,timeend))
+    for(i=1:n)
+        label=uilabel(f,'Text',strcat(char(96+i),':',num2str(solution(i))),'FontSize',12,'Position',[20 730-(n+3+i)*20 145 60]);
+    end
+    label=uilabel(f,'Text',strcat('time taken to calculate:',num2str(time)),'FontSize',12,'Position',[20 730-(2*n+3+1)*20 300 60]);
 end
 function getNsiedle(buttonN,textN,f)
     solution2=[];
@@ -130,7 +160,6 @@ function geteqnsielde(calculate_btn,f,n,eqn,stringat,unknowns,maxiterations,app_
     app_error=str2num(app_error.Value);
     flag=1;
     counter=0;
-    timestart=datevec(datestr(now,'dd-mm-yyyy HH:MM:SS.FFF'));
     for(i=1:n)
         if i==1
             equations=[string(eqn(i).Value)];
@@ -158,6 +187,7 @@ function geteqnsielde(calculate_btn,f,n,eqn,stringat,unknowns,maxiterations,app_
             matrixOfCoeff(j,n+1) = b(findIfZero(1));
         end
     end
+     timestart=datevec(datestr(now,'dd-mm-yyyy HH:MM:SS.FFF'));
     while(flag==1 && counter<maxiterations)
         counterTable=1;
         for i=1:n
@@ -196,11 +226,11 @@ function geteqnsielde(calculate_btn,f,n,eqn,stringat,unknowns,maxiterations,app_
     uit=uitable(f,'ColumnName',columnNames,'data',tableElements,'position',[20 750-(2*n+4)*20-300 750 300]);
     timeend=datevec(datestr(now,'dd-mm-yyyy HH:MM:SS.FFF'));
     time=abs(etime(timestart,timeend))
-%     for i=1:n
-%         label3=uilabel(f,'Text',strcat(char(96+i),':',num2str(solution2(i))),'FontSize',12,'Position',[20 750-(2*n+4+countit+(counter)*n+i)*20 1000 20]);
-%     end
-%     label4=uilabel(f,'Text',strcat('number of iterations',num2str(counter)),'FontSize',12,'Position',[20 750-(2*n+4+countit+(counter)*n+i+1)*20 1000 20]);
-%     label4=uilabel(f,'Text',strcat('time taken to calculate:',num2str(time)),'FontSize',12,'Position',[20 750-(2*n+4+countit+(counter)*n+i+2)*20 1000 20]);
+    for i=1:n
+        label3=uilabel(f,'Text',strcat(char(96+i),':',num2str(solution2(i))),'FontSize',12,'Position',[20 750-(2*n+4+1)*20-300 1000 20]);
+    end
+    label4=uilabel(f,'Text',strcat('number of iterations',num2str(counter)),'FontSize',12,'Position',[20 750-(2*n+4+2)*20-300 1000 20]);
+    label4=uilabel(f,'Text',strcat('time taken to calculate:',num2str(time)),'FontSize',12,'Position',[20 750-(2*n+4+3)*20-300 1000 20]);
     
 end
 function getN(buttonN,textN,f)
@@ -225,7 +255,6 @@ function getN(buttonN,textN,f)
         calculate_btn=uibutton(f,'FontSize',14,'text','Calculate','Position',[220 750-(n+1)*30 100 20],'ButtonPushedFcn',@(calculate_btn,event) geteqn(calculate_btn,f,n,eqn,stringat,unknowns));
 end
 function geteqn(calculate_btn,f,n,eqn,stringat,unknowns)
-    timestart=datevec(datestr(now,'dd-mm-yyyy HH:MM:SS.FFF'));
     for(i=1:n)
         if i==1
             equations=[string(eqn(i).Value)];
@@ -254,6 +283,7 @@ function geteqn(calculate_btn,f,n,eqn,stringat,unknowns)
             matrixOfCoeff(j,n+1) = b(findIfZero(1));
         end
     end
+    timestart=datevec(datestr(now,'dd-mm-yyyy HH:MM:SS.FFF'));
     for i=1:n-1
         if matrixOfCoeff(i,i)==0
             fprintf("can't solve");
